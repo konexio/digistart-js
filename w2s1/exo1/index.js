2c096405b0af9aa091dd0a65bc345dd8a958db82
SimonGodefroid <simon.godefroid@gmail.com>
2019-03-01 20:13:40 +0100

    [simon] Add folder structure and content for w2

---
+++ w2s1/exo1/index.js
@@ -0,0 +1,27 @@
+/* Exercice 1 - Change la couleur de fond du body */
+
+// Cette fonction est executée à chaque fois que l'utilisateur va changer
+// la valeur de l'élément "select" dans le HTML, c'est à dire, à chaque fois
+// qu'il va "changer" d'option... c'est pourquoi elle est liée à l'événement onChange
+// dans le code HTML.
+function selectChange() {
+
+  // cette variable récupère la valeur de l'option sélectionnée.
+  var color = document.getElementById('colors').value;
+
+  // Cette ligne de code permet d'afficher le choix de l'utilisateur dans la console du navigateur.
+  console.log("l'utilisateur a choisi: ", color);
+
+  // Instructions:
+  // Écrire le code pour changer la couleur de fond du body en fonction de
+  // la valeur qu'a choisi l'utilisateur, en utilisant des conditions (if)
+
+  // Pour changer la couleur de fond du body, il faut utiliser le code ci-dessous:
+  // document.body.style.backgroundColor = '...';
+
+
+  /* MON CODE EN DESSSOUS */
+
+  /* MON CODE AU DESSUS */
+
+};

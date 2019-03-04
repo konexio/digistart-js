6fe8ff1a776644d5f7125ce4036e2037a0fb3ae2
Dara Hak <dara.hak@gmail.com>
2019-03-04 02:20:45 +0100

    [WIP] Rework of w1s2's exo2

---
+++ w1s2/exo2/exo2.js
@@ -0,0 +1,19 @@
+/* Une fonction pour gérer l'addition de la calculatrice */
+
+function add() {}
+
+/* Une fonction pour gérer la soustraction de la calculatrice */
+
+function subtract() {}
+
+/* Une fonction pour gérer la multiplication de la calculatrice */
+
+function multiply() {}
+
+/* Une fonction pour gérer la division de la calculatrice */
+
+function divide() {}
+
+/* Une fonction pour afficher le résultats de la calculatrice */
+
+function displayRes(resId, total) {}

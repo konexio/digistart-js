dbc2f99696bd6fb10f4c97000cb36094d763d5b7
Dara Hak <dara.hak@gmail.com>
2019-03-04 11:29:08 +0100

    [w1s2] Bonus exercise: add title to HTML page and put back event arg

--- w1s2/bonus/bonus.js
+++ w1s2/bonus/bonus.js
@@ -10,6 +10,6 @@ function areEqual(firstPassword, secondPassword) {
   // TODO: Écrire une fonction qui vérifie que les deux mots de passe passés en paramètres, sont identiques.
 }
 
-function validateForm() {
+function validateForm(event) {
   // TODO: Écrire une fonction qui va appeler les fonctions précédentes pour vérifier la validité de chaque champ du formulaire.
 }

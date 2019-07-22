33c89db79db46ce22e670f8ab6b126e6d3bd5e41
SimonGodefroid <simon.godefroid@gmail.com>
2019-07-22 08:24:58 +0200

    [simon] Add exercises structure for w1s2

---
+++ w1s2/functions/main.js
@@ -0,0 +1,30 @@
+/* LES FONCTIONS */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les fonctions');
+
+// EX 1
+// CONSIGNE
+// Créer et exécuter une fonction sayHello qui imprime "Bonjour" dans la console
+
+// votre code en dessous
+// ...
+// votre code au dessus
+
+// EX 2
+// CONSIGNE
+// Créer et exécuter une fonction sayHelloName qui prend en paramètre le nom d'une personne et
+// qui imprime "Bonjour " suivi du nom de la personne dans la console
+// Par exemple "Bonjour Dara"
+
+// votre code en dessous
+// ...
+// votre code au dessus
+
+// EX 3
+// CONSIGNE
+// Créer et exécuter une fonction addTwo qui prend en paramètre un nombre et qui
+// retourne ce nombre + 2
+
+// votre code en dessous
+// ...
+// votre code au dessus
---
+++ w1s2/types/main.js
@@ -0,0 +1,4 @@
+/* LES TYPES */
+console.log("Bienvenue dans la console javascript");
+console.log("Vous êtes dans les exercices sur les types");
+// Créer et exécuter une fonction

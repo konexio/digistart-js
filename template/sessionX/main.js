989ab4db249627dd51dbf3e25c361bd896f3b4fe
SimonGodefroid <simon.godefroid@gmail.com>
2019-07-26 08:31:26 +0200

    [simon] Add template for session

---
+++ template/sessionX/main.js
@@ -0,0 +1,11 @@
+/* <#TEMPLATE NOM DE LA SECTION> */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur <#TEMPLATE NOM DE LA SECTION>');
+
+// EX 1
+// CONSIGNE
+// #TEMPLATE MA CONSIGNE
+
+// votre code en dessous
+// ...
+// votre code au dessus

937cc37230092b5ce0ff8ed4f8e9d3dff49c4564
Yveline Say <sayyveline@gmail.com>
2019-08-14 11:55:23 +0200

    [s1] Rework exercises

---
+++ session1/exo1/main.js
@@ -0,0 +1,16 @@
+console.log('Bienvenue dans votre premier exercice JavaScript !')
+
+// ------ TODO 1 ------ 
+// Afficher dans la console le mois actuel
+// en assignant la bonne valeur à la variable `month` 
+
+var month;
+
+
+
+// ------ TODO 2 ------ 
+// Afficher dans la console le mois prochain
+// en réassignant la variable `month`
+
+
+

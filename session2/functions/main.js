4efb03df159ef2df7af672789642bead91bf254f
Yveline Say <sayyveline@gmail.com>
2019-09-03 20:14:09 +0200

    update format and add methods exercises

--- session2/functions/main.js
+++ session2/functions/main.js
@@ -2,29 +2,37 @@
 console.log('Bienvenue dans la console javascript');
 console.log('Vous êtes dans les exercices sur les fonctions');
 
-// EX 1
-// CONSIGNE
-// Créer et exécuter une fonction sayHello qui imprime "Bonjour" dans la console
+// ------ TODO 1 ------ 
+// 1. Créer une fonction sayHello qui imprime "Bonjour" dans la console
 
-// votre code en dessous
-// ...
-// votre code au dessus
 
-// EX 2
-// CONSIGNE
-// Créer et exécuter une fonction sayHelloName qui prend en paramètre le nom d'une personne et
-// qui imprime "Bonjour " suivi du nom de la personne dans la console
-// Par exemple "Bonjour Dara"
 
-// votre code en dessous
-// ...
-// votre code au dessus
 
-// EX 3
-// CONSIGNE
-// Créer et exécuter une fonction addTwo qui prend en paramètre un nombre et qui
+// 2. L'exécuter
+
+
+
+
+// ------ TODO 2 ------ 
+// 1. Créer une fonction sayHelloName qui prend en paramètre le nom d'une personne, et
+// qui imprime "Bonjour " suivi du nom de la personne dans la console.
+
+
+
+
+// 2. L'exécuter avec le paramètre de votre choix.
+
+
+
+
+// ------ TODO 3 ------ 
+// 1. Créer une fonction addTwo qui prend en paramètre un nombre et qui
 // retourne ce nombre + 2
 
-// votre code en dessous
-// ...
-// votre code au dessus
+
+
+
+// 2. L'exécuter avec le paramètre de votre choix.
+
+
+

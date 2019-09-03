d969167ff2f7abf43a4fe3b3f0afbc5b17d42c7d
Yveline Say <yvelinesay@Yvelines-MacBook-Pro.local>
2019-09-03 20:14:09 +0200

    update format and add methods exercises

--- session2/types/main.js
+++ session2/types/main.js
@@ -2,47 +2,45 @@
 console.log('Bienvenue dans la console javascript');
 console.log('Vous êtes dans les exercices sur les types');
 
-// EX 1 - Déclarer des variables
-// CONSIGNE
-// Écrire 4 variables, 1 en dessous de chaque ligne avec leurs valeurs.
+// ------ TODO 1 ------ 
+// Écrire 4 variables, 1 en dessous de chaque ligne avec leurs valeurs :
+// - une variable qui s'appelle firstName de type string qui contient votre prénom
 
-// une variable qui s'appelle firstName de type string qui contient votre prénom
-// votre code en dessous
-// ...
-// votre code au dessus
 
-// une variable qui s'appelle age de type number qui contient votre age
-// votre code en dessous
-// ...
-// votre code au dessus
+// - une variable qui s'appelle age de type number qui contient votre age
 
-// une variable qui s'appelle likeFootball de type boolean qui indique si vous aimez le football ou non
-// votre code en dessous
-// ...
-// votre code au dessus
 
-// une variable qui s'appelle otherStuff de valeur null
-// votre code en dessous
-// ...
-// votre code au dessus
+// - une variable qui s'appelle likeFootball de type boolean qui indique si vous aimez le football ou non
 
-// EX 2 - logger des variables et leur type
-// CONSIGNE
-// Afficher dans la console, un message pour chaque variable, pour aficher la valeur de la variable et son type.
-// pour chaque variable suivre cet exemple de pseudo code
+
+// - une variable qui s'appelle otherStuff de valeur null
+
+
+
+
+// ------ TODO 2 ------ 
+// Afficher dans la console la valeur et le type de chaque variable.
+// Pour chaque variable, suivre cet exemple de pseudo code :
 // "mon prénom, {name}, est de type {type de name}"
 
-// votre code en dessous
-// ...
-// votre code au dessus
 
-// EX 3 - le typage dynamique
-// CONSIGNE
-// Créer une variable appellée myNumber et lui attribuer une valeur numérique (un nombre)
-// Afficher dans la console le type de myNumber
-// Changer la valeur de myNumber à un texte (une chaîne de caractères)
-// Afficher dans la console le type de myNumber
 
-// votre code en dessous
-// ...
-// votre code au dessus
+
+
+
+
+
+
+
+
+
+// ------ TODO 3 ------ 
+// 1. Créer une variable appellée myNumber et lui attribuer une valeur numérique (un nombre)
+// 2. Afficher dans la console le type de myNumber.
+// 3. Changer la valeur de myNumber à un texte (une chaîne de caractères)
+// 4. Afficher dans la console le type de myNumber.
+
+
+
+
+

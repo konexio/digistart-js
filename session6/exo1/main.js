090b4f889a5e6429099a07ceb8eb81effc90c0b6
Yveline Say <yvelinesay@Yvelines-MacBook-Pro.local>
2019-09-04 12:06:29 +0200

    [s6] add exercises

---
+++ session6/exo1/main.js
@@ -0,0 +1,10 @@
+/* LE DOM ET LES ÉVÉNEMENTS */
+console.log('Bienvenue dans la console javascript');
+console.log("Vous êtes dans l'exercice 1 sur le DOM et les événements");
+
+// ------ TODO 1 ------
+// Créer la fonction appelée par le bouton que vous avez créé dans le HTML.
+// Elle devra afficher une alerte avec le texte "Vous avez cliqué sur le bouton !"
+
+
+

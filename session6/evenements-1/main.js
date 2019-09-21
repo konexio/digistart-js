ab4ceb6efe34548a8a7dba5deb693c1424847cd6
Hugo Dalboussiere <hugo.dalboussiere@gmail.com>
2019-09-21 21:19:05 +0200

    add DOM exercises to session 6

--- session6/evenements-1/main.js
+++ session6/evenements-1/main.js
@@ -1,6 +1,6 @@
-/* LE DOM ET LES ÉVÉNEMENTS */
+/* LES ÉVÉNEMENTS */
 console.log('Bienvenue dans la console javascript');
-console.log("Vous êtes dans l'exercice 1 sur le DOM et les événements");
+console.log("Vous êtes dans l'exercice 1 sur les événements");
 
 // ------ TODO 1 ------
 // Créer la fonction appelée par le bouton que vous avez créé dans le HTML.

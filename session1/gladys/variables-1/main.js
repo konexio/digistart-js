4c519e47869c173bcf663d0c984f6322df8f02dc
gbo <guillaume.boussard@gmail.com>
2020-08-31 20:37:34 +0200

    31-08-2020

---
+++ session1/gladys/variables-1/main.js
@@ -0,0 +1,16 @@
+console.log('hello world!');
+
+// TODO: ÉTAPE 1
+// Afficher dans la console le mois actuel
+// en assignant la bonne valeur à la variable `month` ci-dessous.
+// Libre à vous de choisir le format : soit le numéro, soit le nom du mois.
+
+var month = "july";
+month = "august";
+
+// TODO: ÉTAPE 2
+// Afficher dans la console le mois prochain
+// en réassignant la variable `month` ci-dessus,
+// donc sans supprimer le code de l'étape 1 !
+
+console.log(month);
---
+++ session1/konexio/variables-2/js/main.js
@@ -0,0 +1,52 @@
+console.log('Bienvenue dans votre deuxième exercice JavaScript !');
+
+// TODO: ÉTAPE 1
+// Déclarer deux variables nommées "color" et "age"
+// sans leur assigner de valeur.
+
+var color;
+var age;
+
+// TODO: ÉTAPE 2
+// Assigner à la variable color le nom d'une couleur de
+// votre choix. N'oubliez pas les guillemets ""
+// car c'est du texte !
+
+color = "red";
+
+
+// TODO: ÉTAPE 3
+// Assigner à la variable age votre âge. Ne mettez
+// pas de guillemets "" car c'est un nombre.
+
+age = 26;
+
+// TODO: ÉTAPE 4
+// Afficher la valeur de ces deux variables
+// dans la console du navigateur.
+
+console.log(color);
+console.log(age);
+
+// TODO: ÉTAPE 5
+// Remplacer la valeur de la variable color par une autre couleur.
+
+color = "green";
+
+// TODO: ÉTAPE 6
+// Afficher de nouveau la variable color dans la console.
+
+console.log(color);
+
+// TODO: ÉTAPE 7 BONUS
+// Créer une nouvelle variable agePlusUn avec
+// la valeur de l'âge que vous aurez l'année prochaine,
+// en réutilisant la valeur stockée dans la variable age
+// et afficher sa valeur dans la console.
+
+
+var agePlusOne;
+
+agePlusOne = 26 + 1;
+
+console.log(agePlusOne);

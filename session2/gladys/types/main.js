0e8d83d1abadd770b56064ae110bd27413d38308
Olivier Jolit <olivier.jolit@pianoctal.com>
2020-09-02 19:46:22 +0200

    Start session

---
+++ session2/gladys/types/main.js
@@ -0,0 +1,96 @@
+/* LES TYPES */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les types');
+
+// ------ TODO 1 ------ 
+// TODO: ÉTAPE 1
+// Écrire 4 variables, 1 en dessous de chaque ligne avec leurs valeurs :
+// - une variable qui s'appelle firstName de type string qui contient votre prénom
+
+
+
+// TODO: ÉTAPE 2
+// - une variable qui s'appelle age de type number qui contient votre age
+
+
+
+// TODO: ÉTAPE 3
+// - une variable qui s'appelle likeFootball de type boolean qui indique si vous aimez le football ou non
+
+
+
+// TODO: ÉTAPE 4
+// - une variable qui s'appelle otherStuff de valeur null
+
+
+
+
+// ------ TODO 2 ------ 
+// TODO: ÉTAPE 1
+// Afficher dans la console la valeur et le type de chaque variable.
+// Pour chaque variable, suivre cet exemple de pseudo code :
+// "mon prénom, {name}, est de type {type de name}"
+
+
+
+
+
+
+
+
+// ------ TODO 3 ------ 
+// TODO: ÉTAPE 1
+// Créer une variable appellée myNumber et lui attribuer une valeur numérique (un nombre)
+
+
+
+
+// TODO: ÉTAPE 2
+// Afficher dans la console le type de myNumber.
+
+
+
+
+// TODO: ÉTAPE 3
+// Changer la valeur de myNumber à une string (une chaîne de caractères)
+
+
+
+
+// TODO: ÉTAPE 4
+// Afficher dans la console le type de myNumber.
+
+
+
+
+
+// ------ TODO 4 ------ 
+// TODO: ÉTAPE 1
+// Ecrire une 1ère variable qui contient la chaîne de caractères "10".
+
+
+
+
+// TODO: ÉTAPE 2
+// Ecrire une 2ème variable qui contient la chaîne de caractères "5".
+
+
+
+
+// TODO: ÉTAPE 3
+// Ecrire une 3ème variable qui contient la somme (c = a + b) de la 1ère et la 2ème variable.
+
+
+
+
+// TODO: ÉTAPE 4
+// Afficher dans la console la valeur et le type de la 3ème variable.
+
+
+
+
+// TODO: ÉTAPE 5
+// Que remarque-t-on ? Est-ce le type et le résultat attendu ? Pourquoi ?
+
+
+

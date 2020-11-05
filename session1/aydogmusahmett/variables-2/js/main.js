0440a95f058df6a0c506f75846dd74280f3dfdb6
Olivier Jolit <olivier.jolit@pianoctal.com>
2020-11-05 19:28:55 +0100

    Init

---
+++ session1/aydogmusahmett/variables-2/js/main.js
@@ -0,0 +1,30 @@
+console.log('Bienvenue dans votre deuxième exercice JavaScript !');
+
+// TODO: ÉTAPE 1
+// Déclarer deux variables nommées "color" et "age"
+// sans leur assigner de valeur.
+
+// TODO: ÉTAPE 2
+// Assigner à la variable color le nom d'une couleur de
+// votre choix. N'oubliez pas les guillemets ""
+// car c'est du texte !
+
+// TODO: ÉTAPE 3
+// Assigner à la variable age votre âge. Ne mettez
+// pas de guillemets "" car c'est un nombre.
+
+// TODO: ÉTAPE 4
+// Afficher la valeur de ces deux variables
+// dans la console du navigateur.
+
+// TODO: ÉTAPE 5
+// Remplacer la valeur de la variable color par une autre couleur.
+
+// TODO: ÉTAPE 6
+// Afficher de nouveau la variable color dans la console.
+
+// TODO: ÉTAPE 7 BONUS
+// Créer une nouvelle variable agePlusUn avec
+// la valeur de l'âge que vous aurez l'année prochaine,
+// en réutilisant la valeur stockée dans la variable age
+// et afficher sa valeur dans la console.

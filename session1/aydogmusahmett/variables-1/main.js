c8916ad974d49720380c480fca93a56ba2044114
Olivier Jolit <olivier.jolit@pianoctal.com>
2020-11-05 21:05:18 +0100

    5 Nov

--- session1/aydogmusahmett/variables-1/main.js
+++ session1/aydogmusahmett/variables-1/main.js
@@ -6,8 +6,14 @@ console.log('Bienvenue dans votre premier exercice sur JavaScript !');
 // Libre à vous de choisir le format : soit le numéro, soit le nom du mois.
 
 var month;
+month = "Novembre";
+console.log(month);
 
 // TODO: ÉTAPE 2
 // Afficher dans la console le mois prochain
 // en réassignant la variable `month` ci-dessus,
 // donc sans supprimer le code de l'étape 1 !
+
+month ="Decembre";
+console.log(month);
+

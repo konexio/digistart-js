98840f1ff7ec1ad3ef79f635e488d0c2ad618d19
Charles Jambon <charles.jambon@fitle.com>
2020-11-24 18:36:13 +0100

    Session 3 preparation

---
+++ session3/Charles/operateurs-logiques/main.js
@@ -0,0 +1,67 @@
+/* LES OPERATEURS LOGIQUES */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les opérateurs logiques');
+
+// ------ TODO 1 ------ 
+// Écrire le code pour obtenir la valeur booléenne des cas suivants:
+// 1. faux ou faux
+
+
+
+// 2. vrai et vrai
+
+
+
+// 3. pas faux
+
+
+
+// 4. pas pas vrai
+
+
+
+// 5. pas pas faux
+
+
+
+
+// ------ TODO 2 ------ 
+// 1. Créer une variable var1 qui contient un booléen (true ou false).
+
+
+
+
+// 2. Créer une variable var2 qui contient la valeur inverse de var1 (opérateur PAS).
+
+
+
+
+// 3. Afficher dans la console l'opération logique var1 ET var2 et vérifier que le résultat
+// affiché est bien égal à false.
+
+
+
+
+
+// ------ TODO 3 ------ 
+// Écrire le code pour afficher les valeurs booléennes avec la fonction Boolean() pour:
+// 1. "a"
+
+
+
+// 2. ""
+
+
+
+// 3. 1
+
+
+
+// 4. 0
+
+
+
+// 5. null
+
+
+

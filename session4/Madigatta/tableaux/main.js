b7bd6a921bfc2a8d1938d2b77349392083cb291c
Charles Jambon <charles.jambon@fitle.com>
2020-12-01 18:51:01 +0100

    Preparation class of 01/12

---
+++ session4/Madigatta/tableaux/main.js
@@ -0,0 +1,85 @@
+/* LES TABLEAUX (ARRAYS) */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les tableaux (arrays)');
+
+// ------ TODO 1 ------ 
+var prices = [12, 3, 25, 4, 987, 22, 15];
+
+// 1. En accédant aux éléments du tableau, faire en sorte que le chiffre 4 s'affiche dans la console.
+
+
+
+// 2. Afficher dans la console la longueur du tableau "prices".
+
+
+
+// 3. Ajouter un prix au tableau et afficher le tableau dans la console.
+
+
+
+
+// ------ TODO 2 ------ 
+var random = ["A", null, 123, "hello", true];
+
+// 1. En accédant aux éléments du tableau, remplacer un élément du tableau.
+
+
+
+// 2. Afficher le tableau dans la console.
+
+
+
+
+// ------ TODO 3 ------ 
+var array = [1, 2, 3, 5, 6, 7, 9, 10];
+
+// 1. Ajouter dans le tableau les éléments 4 et 8 manquants grâce à la méthode splice afin d'obtenir le tableau suivant :
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Documentation de la méthode splice : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/splice
+
+
+
+// 2. Afficher le tableau dans la console.
+
+
+
+// 3. Ajouter la valeur 11 à la fin du tableau.
+
+
+
+// 4. Ajouter la valeur 0 au début du tableau.
+
+
+
+// 5. Afficher le tableau dans la console.
+
+
+
+
+// ------ TODO 4 ------ 
+var array = ["B", "A", "C", "E", "D"];
+
+// 1. Trier le tableau dans l'ordre alphabétique croissant grâce à la méthode sort.
+
+
+
+// 2. Utiliser la méthode join pour regrouper les éléments du tableau en une string.
+
+
+
+// 3. Afficher le tableau dans la console et vérifier qu'on obtient la string "ABCDE".
+
+
+
+
+// ------ TODO 5 ------ 
+var array = ["Michael", "Jordan", "Laeticia", "Julie"];
+
+// 1. Ecrire une fonction qui renvoie le dernier élément du tableau passé en paramètre.
+
+
+
+// 2. Appeller la fonction avec le tableau array et vérifier que l'élément "Julie" est bien renvoyé en l'affichant dans la console.
+
+
+

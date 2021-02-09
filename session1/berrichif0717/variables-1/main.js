a244c82ee83d41f1e853337bec73824e9b535b1d
gbo <guillaume.boussard@gmail.com>
2021-02-09 20:33:01 +0100

    09-02-2021

---
+++ session1/berrichif0717/variables-1/main.js
@@ -0,0 +1,20 @@
+console.log('Bienvenue dans votre premier exercice sur JavaScript !');
+console.log('Je commence le langages JavaScript');
+
+// TODO: ÉTAPE 1
+// Afficher dans la console le mois actuel
+// en assignant la bonne valeur à la variable `month` ci-dessous.
+// Libre à vous de choisir le format : soit le numéro, soit le nom du mois.
+
+var month;
+month = "february";
+console.log(month);
+
+// TODO: ÉTAPE 2
+// Afficher dans la console le mois prochain
+// en réassignant la variable `month` ci-dessus,
+// donc sans supprimer le code de l'étape 1 !
+// COUCOU
+
+month = "march";
+console.log(month);
---
+++ session1/mathieu.giard92/variables-1/main.js
@@ -0,0 +1,16 @@
+console.log('Bienvenue dans votre premier exercice sur JavaScript !');
+// TODO: ÉTAPE 1
+// Afficher dans la console le mois actuel
+// en assignant la bonne valeur à la variable `month` ci-dessous.
+// Libre à vous de choisir le format : soit le numéro, soit le nom du mois.
+
+var month;
+month = "February";
+month = "March"
+console.log(month);
+
+// TODO: ÉTAPE 2
+// Afficher dans la console le mois prochain
+// en réassignant la variable `month` ci-dessus,
+// donc sans supprimer le code de l'étape 1 !
+//coucou

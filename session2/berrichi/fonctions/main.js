17ef37c478c853bb003a7f4ce38a265444d6f3de
vpascal <vpascal@altaprofits.fr>
2021-02-11 21:12:32 +0100

    11-02-2021

---
+++ session2/berrichi/fonctions/main.js
@@ -0,0 +1,70 @@
+/* LES FONCTIONS */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les fonctions');
+
+// ------ TODO 1 ------ 
+// TODO: ÉTAPE 1
+// Créer une fonction sayHello qui imprime "Bonjour" dans la console
+
+function sayHello() {
+    console.log("bonjour")
+}
+
+sayHello();
+
+
+// TODO: ÉTAPE 2
+// L'exécuter
+
+
+
+
+// ------ TODO 2 ------ 
+// TODO: ÉTAPE 1
+// Créer une fonction sayHelloName qui prend en paramètre le nom d'une personne, et
+// qui imprime "Bonjour " suivi du nom de la personne dans la console.
+
+function sayHello(name) {
+    console.log("bonjour name ")
+}
+
+
+
+
+
+// TODO: ÉTAPE 2
+// L'exécuter avec le paramètre de votre choix.
+sayHello("faiza");
+
+
+
+
+// ------ TODO 3 ------ 
+// TODO: ÉTAPE 1
+// Créer une fonction addTwo qui prend en paramètre un nombre et qui
+// retourne ce nombre + 2
+
+function addTwo(number) {
+    return 4(number + 2)
+}
+
+
+
+
+
+// TODO: ÉTAPE 2
+// L'exécuter avec le paramètre de votre choix.
+
+(addTwo(3));
+
+
+// ------ TODO 4 ------ 
+// TODO: ÉTAPE 1
+// Créer une fonction displaySum qui prend en paramètre deux nombres entiers
+// et qui affiche la somme de ces deux nombres dans la console.
+
+
+
+
+// TODO: ÉTAPE 2
+// L'exécuter avec le paramètre de votre choix.
